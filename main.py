ccd910809f2d84c70782a588fb44e4c9421176eb
Max Kutovoy <maxkutovoy@yandex.ru>
2022-03-22 13:10:33 +0600

    Setup DialogFlow

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from telegram import Update, ForceReply
 from telegram.ext import (Updater, CommandHandler, MessageHandler, Filters,
                           CallbackContext)
 from pathlib import Path
+from google.cloud import storage
 
 env = Env()
 env.read_env()
@@ -17,7 +18,37 @@ def start(update: Update, context: CallbackContext):
 
 
 def echo(update: Update, context: CallbackContext):
-    update.message.reply_text(update.message.text)
+
+    text = update.message.text
+    answer = detect_intent_texts(text)
+    chat_id = update.message.chat_id
+    context.bot.send_message(chat_id=chat_id,
+                             text=answer)
+
+
+def detect_intent_texts(text):
+    """Returns the result of detect intent with texts as inputs.
+
+    Using the same `session_id` between requests allows continuation
+    of the conversation."""
+    from google.cloud import dialogflow
+    project_id = 'speech-recognition-lyfe'
+    session_id = '479351324'
+    language_code = 'ru'
+
+    session_client = dialogflow.SessionsClient()
+
+    session = session_client.session_path(project_id, session_id)
+
+    text_input = dialogflow.TextInput(text=text, language_code=language_code)
+
+    query_input = dialogflow.QueryInput(text=text_input)
+
+    response = session_client.detect_intent(
+        request={"session": session, "query_input": query_input}
+    )
+
+    return response.query_result.fulfillment_text
 
 
 def main():
